b7b05e542544f86c9f611c636fc7d64e0a7314ff
Giorgio <carscars21@gmail.com>
2023-10-15 20:54:45 +0200

    Update TB with LSR tests

--- S4/shifter_tb.vhdl
+++ S4/shifter_tb.vhdl
@@ -68,8 +68,17 @@ begin
   process
   begin
 
-    -- LSL
+    -- No shift
+    report "Logical Shift Left tests." severity note;
+
     shift_lsl_s <= '1';
+    din_s       <= x"00000001";
+    shift_val_s <= "00000";
+    wait for 1 ns;
+    assert(dout_s = x"00000001") report "Incorrect shift, expected 0x00000002, dout = 0x" & to_hstring(dout_s) severity error;
+    assert(cout_s = '0') report "Incorrect carry out"severity error;
+
+    ---- LSL
     din_s       <= x"00000001";
     shift_val_s <= "00001";
     wait for 1 ns;
@@ -77,7 +86,6 @@ begin
     assert(cout_s = '0') report "Incorrect carry out"severity error;
 
     -- LSL
-    shift_lsl_s <= '1';
     din_s       <= x"00000001";
     shift_val_s <= "00010";
     wait for 1 ns;
@@ -85,12 +93,36 @@ begin
     assert(cout_s = '0') report "Incorrect carry out"severity error;
 
     -- LSL
-    shift_lsl_s <= '1';
     din_s       <= x"80000000";
     shift_val_s <= "00001";
     wait for 1 ns;
     assert(dout_s = x"00000000") report "Incorrect shift, expected 0x00000000, dout = 0x" & to_hstring(dout_s) severity error;
     assert(cout_s = '1') report "Incorrect carry out"severity error;
+
+    ---- LSR
+    report "Logical Shift Right tests." severity note;
+
+    shift_lsl_s <= '0';
+    shift_lsr_s <= '1';
+    din_s       <= x"00000001";
+    shift_val_s <= "00001";
+    wait for 1 ns;
+    assert(dout_s = x"00000000") report "Incorrect shift, expected 0x00000002, dout = 0x" & to_hstring(dout_s) severity error;
+    assert(cout_s = '1') report "Incorrect carry out"severity error;
+
+    -- LSR
+    din_s       <= x"80000000";
+    shift_val_s <= "00001";
+    wait for 1 ns;
+    assert(dout_s = x"40000000") report "Incorrect shift, expected 0x00000004, dout = 0x" & to_hstring(dout_s) severity error;
+    assert(cout_s = '0') report "Incorrect carry out"severity error;
+
+    -- LSR
+    din_s       <= x"80000000";
+    shift_val_s <= "00010";
+    wait for 1 ns;
+    assert(dout_s = x"20000000") report "Incorrect shift, expected 0x00000000, dout = 0x" & to_hstring(dout_s) severity error;
+    assert(cout_s = '0') report "Incorrect carry out"severity error;
 
     -- Clear inputs
     shift_lsl_s <= '0';
