1432fc17be4f3a4e34160691356d4c25e3fb390d
Giorgio <carscars21@gmail.com>
2023-10-20 12:30:50 +0200

    Add some initial values for ALU

--- S4/alu.vhdl
+++ S4/alu.vhdl
@@ -17,11 +17,11 @@ entity Alu is
     ---- Output
     -- Resultat + retenue Carry out
     res  : out std_logic_vector(31 downto 0);
-    cout : out std_logic;
+    cout : out std_logic := '0'; -- Carry out 
     -- Flags
-    z : out std_logic; -- Zero (1 if zero, 0 otherwise)
-    n : out std_logic; -- Negative (1 if negative, 0 if positive)
-    v : out std_logic; -- Overflow (1 if overflow, 0 otherwise)
+    z : out std_logic := '0'; -- Zero (1 if zero, 0 otherwise)
+    n : out std_logic := '0'; -- Negative (1 if negative, 0 if positive)
+    v : out std_logic := '0'; -- Overflow (1 if overflow, 0 otherwise)
 
     ---- 
     -- Tensions
