80204145656e037adf82e42c095facecfbf7a443
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2023-11-27 18:00:00 +0100

    Modify comment

--- S4/adder_4bit.vhdl
+++ S4/adder_4bit.vhdl
@@ -16,7 +16,7 @@ end adder4;
 architecture behavioural of adder4 is
   signal temp : std_logic_vector(4 downto 0);
 begin
-  -- Convert signals to 33 bits for carry in
+  -- Convert signals to 4+1 = 5 bits to get carry out (MSB)
   temp <= std_logic_vector(unsigned('0' & a) + unsigned('0' & b) + unsigned'('0' & cin));
   sum  <= temp(3 downto 0);
   cout <= temp(4);
